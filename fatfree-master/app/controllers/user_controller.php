c03aedbb0c7c0606e360c29666569115f4489c5b
Dockers <glogowski.victor@gmail.com>
2014-02-06 17:46:03 +0100

    inscription boxeur

--- fatfree-master/app/controllers/user_controller.php
+++ fatfree-master/app/controllers/user_controller.php
@@ -4,7 +4,6 @@ class user_controller extends controller{
     
   public function __construct(){
     parent::__construct();
-    $this->tpl=array('sync'=>'main.html');
   }
   
   function home(){
@@ -55,7 +54,26 @@ class user_controller extends controller{
 
     function inscription($f3){
       $model=new user_model();
-      $f3->set('nom',$model->inscription($f3,array('nom'=>$f3->get('POST.nom'))));
+      $f3->set('inscription',$model->inscription(
+        $f3,
+        array(
+          'nom'=>$f3->get('POST.nom'),
+          'prenom'=>$f3->get('POST.prenom'),
+          'password'=>$f3->get('POST.password'),
+          'anniversaire'=>$f3->get('POST.anniversaire'),
+          'age'=>$f3->get('POST.poids'),
+          'sexe'=>$f3->get('POST.sexe'),
+          'email'=>$f3->get('POST.email'),
+          'photo'=>$f3->get('POST.photo'),
+          'classe'=>$f3->get('POST.classe'),
+          'dispo'=>$f3->get('POST.dispo'),
+          'poids'=>$f3->get('POST.poids'),
+          'taille'=>$f3->get('POST.taille'),
+          'main'=>$f3->get('POST.main'),
+          'palmares'=>$f3->get('POST.palmares')
+          )
+        )
+      );
       echo View::instance()->render('main.html');
     }
 }
--- fatfree-master/app/models/user_model.php
+++ fatfree-master/app/models/user_model.php
@@ -1,14 +1,60 @@
 <?php
 class user_model extends model{
     
+    private $mapper;
+
     function __construct(){
         parent::__construct();
+        $this->mapper=$this->getMapper('boxeurs');
      }
   
   function inscription($f3,$params){
-    $nom = $this->getMapper('test');
-    $nom->nom=$params['nom'];
-    return $nom->save();
+    $inscrip=$this->mapper;
+    $inscrip->nom=$params['nom'];
+    $inscrip->prenom=$params['prenom'];
+    $inscrip->password=$params['password'];
+    $inscrip->datenaissance=$params['anniversaire'];
+    $inscrip->age=$params['age'];
+    $inscrip->sexe=$params['sexe'];
+    $inscrip->email=$params['email'];
+    $inscrip->photo=$params['photo'];
+    $inscrip->classe=$params['classe'];
+    $inscrip->disponibilite=$params['dispo'];
+    $inscrip->poids=$params['poids'];
+    $inscrip->taille=$params['taille'];
+    $inscrip->main=$params['main'];
+    $inscrip->palmares=$params['palmares'];
+    // $nom = $this->mapper->find('nom="'.$params['nom'].'"');
+    // var_dump('nom : '.$params['nom']);
+    // $prenom = $this->mapper->find('prenom="'.$params['prenom'].'"');
+    // var_dump('prenom : '.$params['prenom']);
+    // $password = $this->mapper->find('password="'.$params['password'].'"');
+    // var_dump('mdp : '.$params['password']);
+    // $anniversaire = $this->mapper->find('datenaissance="'.$params['anniversaire'].'"');
+    // var_dump('anniversaire : '.$params['anniversaire']);
+    // $age = $this->mapper->find('age="'.$params['age'].'"');
+    // var_dump('age : '.$params['age']);
+    // $sexe = $this->mapper->find('sexe="'.$params['sexe'].'"');
+    // var_dump('sexe : '.$params['sexe']);
+    // $email = $this->mapper->find('email="'.$params['email'].'"');
+    // var_dump('mail : '.$params['email']);
+    // $photo = $this->mapper->find('photo="'.$params['photo'].'"');
+    // var_dump('photo : '.$params['photo']);
+    // $classe = $this->mapper->find('classe="'.$params['classe'].'"');
+    // var_dump('classe : '.$params['classe']);
+    // $dispo = $this->mapper->find('disponibilite="'.$params['dispo'].'"');
+    // var_dump('dispo : '.$params['dispo']);
+    // $poids = $this->mapper->find('poids="'.$params['poids'].'"');
+    // var_dump('poids : '.$params['poids']);
+    // $taille = $this->mapper->find('taille="'.$params['taille'].'"');
+    // var_dump('taille : '.$params['taille']);
+    // $main = $this->mapper->find('main="'.$params['main'].'"');
+    // var_dump('main : '.$params['main']);
+    // $palmares = $this->mapper->find('palmares="'.$params['palmares'].'"');
+    // var_dump('palmares : '.$params['palmares']);
+    // $this->mapper->copyFrom('POST');
+    $inscrip->save();
+    // return $this->mapper->save();
   }
 
   // function inscription($f3, $params){
