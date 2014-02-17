ed5782d13d44adb72d81b828ed29de13584897d3
Lucas Fayolle <lucas.fayolle@orange.fr>
2014-02-17 18:50:20 +0100

    Popin finies avec tout le contenu

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@
 
 	// Show or hide the overlay
 	$('footer ul li a').on('click', showPopin);
-	$('#popin a.closePopin').on('click', closePopin);
+	$('#popin').on('click', "a.closePopin", closePopin);
 
 
     function hoverHome() {
@@ -75,6 +75,19 @@
 	function showPopin(e) {
 		e.preventDefault();
 		TweenLite.to(overlay, 0.3, {opacity:1,display:'block',ease:Power4.easeInOut});
+		var _this = $(this),
+			popin = $('#popin'),
+			content = _this.data("content");
+
+		if(content=="cgu") {
+			popin.html("<h3>Conditions Générales d'Utilisation</h3><a href='#' class='closePopin'><i class='icon-cross_mark'></i></a><p>Le présent document a pour objet de définir les modalités et conditions dans lesquelles d’une part,  monsieur Noguier Robin, ci-après dénommé l’EDITEUR, met à la disposition de ses utilisateurs le site, et les services disponibles sur le site et d’autre part, la manière par laquelle l’utilisateur accède au site et utilise ses services.</br>Toute connexion au site est subordonnée au respect des présentes conditions.</br>Pour l’utilisateur, le simple accès au site de l’EDITEUR à l’adresse URL suivante www.labagarre.fr  implique l’acceptation de l’ensemble des conditions décrites ci-après.</br></br></p><h4>Propriété intellectuelle</h4><h5>Variante 1</h5><p>La structure générale du site labagarre.fr , ainsi que les textes, graphiques, images, sons et  vidéos la composant, sont la propriété de l'éditeur ou de ses partenaires. Toute représentation et/ou reproduction et/ou exploitation partielle ou totale des contenus et services proposés par le site labagarre.fr , par quelque procédé que ce soit, sans l'autorisation préalable et par écrit de  monsieur Noguier Robin  et/ou de ses partenaires est strictement interdite et serait susceptible de constituer une contrefaçon au sens des articles L 335-2 et suivants du Code de la propriété intellectuelle.</br>Les marques 'labagarre', 'labagarre.fr' sont des marques déposées par labagarre.fr .Toute représentation et/ou reproduction et/ou exploitation partielle ou totale de ces marques, de quelque nature que ce soit, est totalement prohibée.</br></p><h5>Variante 2</h5><p>Aucune reproduction, même partielle prévue à l’article L.122-5 du Code de la propriété intellectuelle, ne peut être faite de ce site sans l’autorisation du directeur de publication.</p><h5>Variante 3</h5><p>Tous les éléments de ce site, y compris les documents téléchargeables, sont libres de droit. A l’exception de l’iconographie, la reproduction des pages de ce site est autorisée à la condition d’y mentionner la source. Elles ne peuvent être utilisées à des fins commerciales et publicitaires.</p>");
+		} 
+		if(content=="contact") {	
+			popin.html("<h3>Contact</h3><a href='#' class='closePopin'><i class='icon-cross_mark'></i></a><p style='text-align:center;'>Pour toutes information supplémentaire, suggestion ou autre, nous vous invitons à nous contacter </br> à l’adresse suivante:</p></br><h4 style='text-align:center;'>contact@labagarre.fr</h4>");
+		} 
+		if(content=="who") {
+			popin.html("<h3>Qui sommes nous?</h3><a href='#' class='closePopin'><i class='icon-cross_mark'></i></a><p>Labagarre.fr est une plateforme de mise en relation des différents acteurs de l’univers des sports de combat. Son but est de faciliter et diversifier l’organisation de compétitions, entraînements et autres évènements sportifs en regroupant un public écléctique.</p><p>Simples amateurs, professionnels, entraîneurs, managers, icônes du noble art ou experts en arts martiaux, ceci est votre site. Un site pour vous, pour vos partenaires d’entraînement et vos adversaires, mais surtout par vous.Car c’ est de votre activité sur labagarre.fr que dépendront l’ évolution, la philosophie et l’ image véhiculée par les sports de combat.</p><p>Toute l’équipe de labagarre.fr vous souhaite une excellente navigation ainsi que de beaux combats.</p>");
+		}
 	}
 	function closePopin(e) {
 		e.preventDefault();
