54cc07b827ddb30d22ff55a29c9e7e818068a34e
Dockers <glogowski.victor@gmail.com>
2014-02-26 14:39:03 +0100

    script sql bdd

--- php_labagarre.sql
+++ php_labagarre.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le : Jeu 13 Février 2014 à 15:02
+-- Généré le : Mer 26 Février 2014 à 13:38
 -- Version du serveur: 5.5.20
 -- Version de PHP: 5.3.10
 
@@ -75,14 +75,15 @@ CREATE TABLE IF NOT EXISTS `entraineurs` (
   UNIQUE KEY `email_entraineur_UNIQUE` (`email_entraineur`),
   KEY `FK_boxeur_idx` (`boxeurs_entraineur`),
   KEY `FK_club_idx` (`club_entraineur`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `entraineurs`
 --
 
 INSERT INTO `entraineurs` (`id_entraineur`, `nom_entraineur`, `prenom_entraineur`, `email_entraineur`, `password_entraineur`, `sexe_entraineur`, `codepostal_entraineur`, `naissance_entraineur`, `statut_entraineur`, `sport_entraineur`, `diplome_entraineur`, `club_entraineur`, `palmares_entraineur`, `images_entraineur`, `videos_entraineur`, `boxeurs_entraineur`) VALUES
-(1, 'test', 'test', 'test@test.fr', 'test', 1, 93100, '2014-02-07', 'test', 'test', 'Diplome 2', 2, 'test', '1418374_240410762783621_2076519400_n.jpg', '1418374_240410762783621_2076519400_n.jpg', 13);
+(1, 'test', 'test', 'test@test.fr', 'test', 1, 93100, '2014-02-07', 'test', 'test', 'Diplome 2', 2, 'test', '1418374_240410762783621_2076519400_n.jpg', '1418374_240410762783621_2076519400_n.jpg', 13),
+(2, 'efefezf', 'ezfzef', 'zefze@regrg.rz', 'zrgzrg', 1, 95466, '2014-02-01', 'teh', 'et', 'Diplome 1', 1, 'ergergergergreg', NULL, NULL, 14);
 
 -- --------------------------------------------------------
 
@@ -141,6 +142,23 @@ INSERT INTO `managers` (`id_managers`, `nom_manager`, `prenom_manager`, `email_m
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `messagerie`
+--
+
+CREATE TABLE IF NOT EXISTS `messagerie` (
+  `id_message` int(11) NOT NULL AUTO_INCREMENT,
+  `auteur_message` int(11) NOT NULL,
+  `destinataire_message` int(11) NOT NULL,
+  `contenu_message` text NOT NULL,
+  `date_message` datetime NOT NULL,
+  PRIMARY KEY (`id_message`),
+  KEY `FK_auteur_idx` (`auteur_message`),
+  KEY `FK_destinataire_sportif_idx` (`destinataire_message`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `sportifs`
 --
 
@@ -169,7 +187,7 @@ CREATE TABLE IF NOT EXISTS `sportifs` (
   PRIMARY KEY (`id_sportif`),
   UNIQUE KEY `email_sportif_UNIQUE` (`email_sportif`),
   KEY `FK_club_idx` (`club_sportif`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='contient tout les sportifs' AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='contient tout les sportifs' AUTO_INCREMENT=23 ;
 
 --
 -- Contenu de la table `sportifs`
@@ -177,7 +195,16 @@ CREATE TABLE IF NOT EXISTS `sportifs` (
 
 INSERT INTO `sportifs` (`id_sportif`, `nom_sportif`, `prenom_sportif`, `email_sportif`, `password_sportif`, `sexe_sportif`, `codepostal_sportif`, `naissance_sportif`, `statut_sportif`, `sport_sportif`, `classe_sportif`, `poids_sportif`, `taille_sportif`, `club_sportif`, `palmares_sportif`, `lateralite_sportif`, `victoires_sportif`, `defaites_sportif`, `matchsnuls_sportif`, `images_sportif`, `videos_sportif`) VALUES
 (11, 'test', 'test', 'test@test.fr', 'test', 1, 93100, '2014-02-11', 'test', 'test', 'Intermédiaire', 70, 180, 3, 'test', 0, 1, 1, 1, '1418374_240410762783621_2076519400_n.jpg', '1418374_240410762783621_2076519400_n.jpg'),
-(13, 'test', 'test', 'teseft@test.fr', 'test', 1, 93100, '2014-02-12', 'test', 'test', '', 0, 0, 3, 'test', 0, NULL, NULL, NULL, '1418374_240410762783621_2076519400_n.jpg', '1418374_240410762783621_2076519400_n.jpg');
+(13, 'test', 'test', 'teseft@test.fr', 'test', 1, 93100, '2014-02-12', 'test', 'test', '', 0, 0, 3, 'test', 0, NULL, NULL, NULL, '1418374_240410762783621_2076519400_n.jpg', '1418374_240410762783621_2076519400_n.jpg'),
+(14, 'Ait El Alim', 'Robin', 'robin@test.fr', 'password', 1, 75000, '1990-07-12', 'test', 'boxe', 'Professionnel', 70, 187, 1, 'enorme', 0, 9, 1, 2, NULL, NULL),
+(15, 'Ja', 'Quentin', 'quentin@test.fr', 'password', 1, 94000, '1983-06-15', 'test', 'boxe', 'Professionnel', 70, 210, 3, 'test', 0, 10, 1, 7, NULL, NULL),
+(16, 'Fayolle', 'Lucas', 'lucas@test.fr', 'password', 1, 75000, '1992-06-10', 'test', 'boxe', 'Intermédiaire', 73, 182, 2, 'test', 0, 7, 2, 2, NULL, NULL),
+(17, 'test', 'test', 'etet@zzg.fr', 'test', 1, 94000, '2002-06-13', 'test', 'boxe', 'Professionnel', 40, 187, 3, 'test', 0, 5, 1, 1, NULL, NULL),
+(18, 'rrg', 'srg', 'sge@eh.fr', 'thh', 1, 45200, '2014-02-06', 'eqf', 'hsh', 'Débutant', 70, 180, 1, 'th', 0, 0, 0, 0, NULL, NULL),
+(19, 'sg', 'sg', 'srh@s.fr', 'dh', 1, 78500, '2014-02-04', 'yrr', 'srg', 'Débutant', 70, 180, 1, 'sge', 0, 1, 0, 1, NULL, NULL),
+(20, 'ezg', 'zgr', 'zeg@f.ft', 'af', 1, 45000, '2014-02-13', 'sgr', 'rg', 'Débutant', 70, 180, 1, 'srgsrg', 0, 0, 0, 0, NULL, NULL),
+(21, 'rgrg', 'zgz', 'zeg@eg.fr', 'tyyjty', 0, 44444, '2014-02-13', 'tdhh', 'fgsgsg', 'Débutant', 70, 180, 1, 'sdgsg', 0, 0, 0, 0, NULL, NULL),
+(22, 'etth', 'eth', 'eth@rth.fr', 'rthrt', 0, 56464, '2014-02-13', 'htrrht', 'rgerg', 'Débutant', 70, 180, 1, 'vdbd', 0, 4, 1, 4, NULL, NULL);
 
 --
 -- Contraintes pour les tables exportées
@@ -194,6 +221,17 @@ ALTER TABLE `entraineurs`
 --
 ALTER TABLE `evenements`
   ADD CONSTRAINT `FK_manager` FOREIGN KEY (`id_manager`) REFERENCES `managers` (`id_managers`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `messagerie`
+--
+ALTER TABLE `messagerie`
+  ADD CONSTRAINT `FK_auteur_entraineur` FOREIGN KEY (`auteur_message`) REFERENCES `entraineurs` (`id_entraineur`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FK_auteur_manager` FOREIGN KEY (`auteur_message`) REFERENCES `managers` (`id_managers`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FK_auteur_sportif` FOREIGN KEY (`auteur_message`) REFERENCES `sportifs` (`id_sportif`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FK_destinataire_entraineur` FOREIGN KEY (`destinataire_message`) REFERENCES `entraineurs` (`id_entraineur`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FK_destinataire_manager` FOREIGN KEY (`destinataire_message`) REFERENCES `managers` (`id_managers`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FK_destinataire_sportif` FOREIGN KEY (`destinataire_message`) REFERENCES `sportifs` (`id_sportif`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Contraintes pour la table `sportifs`
