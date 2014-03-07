e25f606766679bb5bc3746b722f3ce08e00334d6
Lucas Fayolle <lucas.fayolle@orange.fr>
2014-03-07 10:33:17 +0100

    css cleaning connexion + js pour infos

--- public/js/main.js
+++ public/js/main.js
@@ -5,9 +5,10 @@
 
 	// Trigger video animation on homepage 
     $('.section').on("mouseenter", hoverHome);
-    
-    // liens sur la home
-    // $('#content.homeContent .section').on('click', linksHome);
+
+    // Pop-in d'information sur la home
+    $('body.signin i.icon-information_black').on('click', connexionPopin);
+    $('body.signin').on('click', 'i.icon-cancel_circle', closePopinConnexion);
 
 	// set overlay to hidden
 	TweenLite.set(overlay, {opacity:0,display:'none'});
@@ -26,10 +27,9 @@
 	// loader preview + animation
 	previewLoading();	
 
+	// Créer les sliders de la sidebar 
     createSliders();
 	
-	
-
 	// Show or hide the overlay
 	$('footer ul li a').on('click', showPopin);
 	$('#popin').on('click', "a.closePopin", closePopin);
@@ -50,13 +50,6 @@
         });
     }
 
-    // fonction pour rediriger vers la bonne page depuis la home
-  //   function linksHome() {
-  //   	var _this = $(this);
-  //   	var url = _this.attr('id') + '.html';
-		// window.location = url;
-  //   }
-
 	// function to set #content size to window size - header
 	function contentSize() {
 		var $window = $(window);
@@ -64,6 +57,7 @@
 
 	}
 
+	// function pour ouvrir la sidebar
 	function openSidebar(e) {
 		e.preventDefault();
 		if(sidebar.hasClass('close')) {
@@ -79,6 +73,7 @@
 		}
 	}
 
+	// animation d'intro sur les pages Search 
 	function animateList() {
 		var item = $('#list ul li');
 		TweenLite.set(item, {transform:"translateX(-50px) translateY(-20px)", opacity:0});
@@ -86,6 +81,7 @@
 			{transform:"translateX(0px) translateY(0px)",opacity:1}, 0.15);
 	}
 
+	// pour montrer ou cacher la popin
 	function showPopin(e) {
 		e.preventDefault();
 		TweenLite.to(overlay, 0.3, {opacity:1,display:'block',ease:Power4.easeInOut});
@@ -108,6 +104,7 @@
 		TweenLite.to(overlay, 0.3, {opacity:0,display:'none',ease:Power4.easeInOut});
 	}
 
+	// Le loader dans la section #preview des pages Search
 	function previewLoading() {
 		var previewContent = $('#preview .wrap'),
 			loader = $('#content .loading-wrap');
@@ -116,6 +113,7 @@
 		TweenLite.to(previewContent, 0.4, {opacity:1,ease:Power1.easeInOut,delay:1.5});
 	}
 
+	// Crée les différents sliders de la page SearchSportif
 	function createSliders() {
 		$('#poid').noUiSlider({
 			 range: [60,120]
@@ -125,8 +123,8 @@
 			,step: 1
 			,serialization: {
 				resolution: 1
-				,to: [[$('.poid .value-span-1'), 'text'],
-				 	  [$('.poid .value-span-2'), 'text']]
+				,to: [[$('.poid .value-span-1'), 'input'],
+				 	  [$('.poid .value-span-2'), 'input']]
 			}
 		});
 		$('#taille').noUiSlider({
@@ -137,8 +135,8 @@
 			,step: 1
 			,serialization: {
 				resolution: 0.01
-				,to: [[$('.taille .value-span-1'), 'text'],
-				 	  [$('.taille .value-span-2'), 'text']]
+				,to: [[$('.taille .value-span-1'), 'input'],
+				 	  [$('.taille .value-span-2'), 'input']]
 			}
 		});
 		$('#distance').noUiSlider({
@@ -149,12 +147,24 @@
 			,step: 1
 			,serialization: {
 				resolution: 1
-				,to: [[$('.distance .value-span-1'), 'text'],
-				 	  [$('.distance .value-span-2'), 'text']]
+				,to: [[$('.distance .value-span-1'), 'input'],
+				 	  [$('.distance .value-span-2'), 'input']]
 			}
 		});
 	}
 
+	// Function pour afficher les infos sup sur la page Connexion
+	function connexionPopin() {
+		var trigger = $('body.signin i.icon-information_black'),
+			trigger2 = $('.info_wrap i'),
+			popin = $('.info_pop');
 
+		trigger.toggle(300);
+		popin.fadeToggle(300);
+	}
+	function closePopinConnexion() {
+		$(this).parent('.info_pop').fadeOut(300);
+		$('body.signin i.icon-information_black').show();
+	}
 
 });
